11959b39c971d9b6179b20afbadba5ebd4948f31
Walter Manjanja <wamanjajhb024@student.wethinkcode.co.za>
2025-10-13 20:48:34 +0200

    initial commit

---
+++ data_structures.py
@@ -0,0 +1,67 @@
+import random
+
+def find_max(numbers: list) -> int:
+    """Find the largest(maximum) number in a list of numbers"""
+
+def find_min(numbers: list) -> int:
+    """Find the smallest(minimum) number in a list of numbers"""
+
+
+def find_average(numbers: list) -> int:
+    """Find the average of a list of numbers int the list 'numbers' and return 
+    it as a float to one decimal point"""
+    
+
+def find_even_pairs(numbers: list) -> int:
+    """Find the neigbouring pairs of numbers that sum up to an even number and 
+    then return a list of tuples with the pairs' index numbers 
+    ie: [1,3,5] returns [(0,1)]
+    """
+
+
+def find_number_of_even_numbers(numbers: list) -> int:
+    """Find the total number of even numbers in the list 'numbers' and return 
+    the number as an integer"""
+
+
+
+def draw_square(height: int) -> None:
+    """
+    Draws a square pattern of asterisks (*) with the given height.
+
+    Parameters:
+        height (int): The height of the square. Must be a positive integer.
+
+    Returns:
+        None: Prints the square pattern directly to console.
+
+    """
+
+# ------------------- First write tests and the make the functions below pass those tests -----------------------
+
+def fibonacci(n):
+    """
+    Find the nth fibonacci number 
+    """
+    pass
+
+def primes(n):
+    """
+    Find the nth prime number
+    """
+    pass
+
+def factorial(n):
+    """
+    Find the factorial of n
+    """
+    pass
+
+def count_vowels(text):
+    """
+    Count how many vowels are there in the sentence
+    """
+
+
+if "__name__" == "__main__":
+    pass
---
+++ tests/test_data_structures.py
@@ -0,0 +1,50 @@
+
+import unittest
+from data_structures import *
+from io import StringIO
+import sys
+
+class TestFunctions(unittest.TestCase):
+    def test_find_max(self):
+        self.assertEqual(find_max([1, 2, 3, 4, 5]), 5)
+        self.assertEqual(find_max([-1, 2, -3, 4, -5, 6, -9, 10]), 10)
+        self.assertEqual(find_max([1, -2, 3, -4, 5, -6, 9, -10, 11, -12, 13, -14, 15, -16, 17, -18, 19, -20]), 19)
+
+    def test_find_min(self):
+        self.assertEqual(find_min([1, 2, 3, 4, 5]), 1)
+        self.assertEqual(find_min([-1, 2, -3, 4, -5, 6, -9, 10]), -9)
+        self.assertEqual(find_min([1, -2, 3, -4, 5, -6, 9, -10, 11, -12, 13, -14, 15, -16, 17, -18, 19, -20]), -20)
+
+    def test_find_average(self):
+        self.assertEqual(find_average([1, 2, 3, 4, 5]), 3.0)
+        self.assertEqual(find_average([-1, 2, -3, 4, -5, 6, -9, 10]), 0.5)
+        self.assertEqual(find_average([1, -2, 3, -4, 5, -6, 9, -10, 11, -12, 13, -14, 15, -16, 17, -18, 19, -20]), -0.5)
+
+    def test_find_even_pairs(self):
+        self.assertEqual(find_even_pairs([1, 2, 3, 4, 5]), [])
+        self.assertEqual(find_even_pairs([1, 2, 3, 4, 5, 6]), [])
+        self.assertEqual(find_even_pairs([6, 2, 3, 5, 9, 4, 1, 11]), [(0, 1), (2, 3), (3, 4), (6, 7)])
+
+    def test_find_total_number_of_even_numbers(self):
+        self.assertEqual(find_number_of_even_numbers([1, 2, 3, 4, 5]), 2)
+        self.assertEqual(find_number_of_even_numbers([1, 2, 3, 4, 5, 6]), 3)
+        self.assertEqual(find_number_of_even_numbers([1, 2, 3, 4, 5, 6, 7, 8]), 4)
+
+    def test_5_square(self):
+        text_capture = StringIO()
+        sys.stdout = text_capture
+        draw_square(10)
+        self.assertEqual(
+            """**********
+**********
+**********
+**********
+**********
+**********
+**********
+**********
+**********
+**********
+""",
+            text_capture.getvalue(),
+        )
